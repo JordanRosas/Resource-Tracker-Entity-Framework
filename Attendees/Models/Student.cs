ef3ed7866694efbb459bfdd04afb7a5b70bd943f
Jordan Rosas <jordanrosas1203@gmail.com>
2019-04-28 14:42:09 -0500

    working commit

---
+++ Attendees/Models/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Attendees.Models
+{
+    public class Student
+    {
+        [Key]
+        public int StudentId { get; set; }
+
+        [Required(ErrorMessage = "* This field is required")]
+        [Column(TypeName = "nvarchar(250)")]
+        [DisplayName("Full Name")]
+        public string FullName { get; set; }
+
+        [Required(ErrorMessage = "* This field is required")]
+        [Column(TypeName = "nvarchar(250)")]
+        public string Email { get; set; }
+
+        [DisplayName("Phone Number")]
+        public int PhoneNumber { get; set; }
+
+        [Column(TypeName = "nvarchar(250)")]
+        public string Location { get; set; }
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Resource Tracker 
+
+> I wanted to practice entity framework to gain a better understadning. I built a small resourse tracking system. For this project I chose to build an application that can track student records.
+
+## What did I learn?
+- How to set up an Entity Framework Project
+- How to Configure an EF project
+- I gained a better understanding of Entity Framework work flow
+- How DB migrations work and the purpose of them
